237b17ebe8847ace79a4815900a235bde73371d4
Vlasov Danil <asingleextrasecond@gmail.com>
2025-05-27 22:11:08 +0800

    CORS test

--- routes/routes.go
+++ routes/routes.go
@@ -56,6 +56,7 @@ func SetupRoutes(db *sql.DB) http.Handler {
 		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders: []string{"Content-Type"},
+		Debug:          true,
 	})
 
 	return c.Handler(router)
