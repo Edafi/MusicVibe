693ff4986437c5332f361bc6f6d475cd2d992dab
Vlasov Danil <asingleextrasecond@gmail.com>
2025-05-29 03:36:15 +0800

    home handler debug

--- middleware/auth.go
+++ middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strings"
 
@@ -19,6 +20,7 @@ func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
 		authHeader := request.Header.Get("Authorization")
 		if !strings.HasPrefix(authHeader, "Bearer ") {
+			log.Println("Middleware: Missing or invalid Authorization header")
 			http.Error(response, "Missing or invalid Authorization header", http.StatusUnauthorized)
 			return
 		}
@@ -28,18 +30,21 @@ func JWTMiddleware(next http.Handler) http.Handler {
 			return []byte(JWT_SECRET), nil
 		})
 		if err != nil || !token.Valid {
+			log.Println("Middleware: Missing or invalid Authorization header")
 			http.Error(response, "Invalid token", http.StatusUnauthorized)
 			return
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
+			log.Println("Middleware: Invalid token claims")
 			http.Error(response, "Invalid token claims", http.StatusUnauthorized)
 			return
 		}
 
 		userID, ok := claims["user_id"].(string)
 		if !ok {
+			log.Println("Middleware: Invalid user ID")
 			http.Error(response, "Invalid user ID", http.StatusUnauthorized)
 			return
 		}
