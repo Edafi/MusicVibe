8ed2617d4944077c95e4761f47a4b6616f348db3
Vlasov Danil <asingleextrasecond@gmail.com>
2025-06-01 03:38:49 +0800

    changed query for search

--- handlers/search.go
+++ handlers/search.go
@@ -96,13 +96,15 @@ func (handler *SearchHandler) SearchTracks(response http.ResponseWriter, request
 		return
 	}
 	query := `
-	SELECT t.id, t.musician_id, t.album_id, t.title, t.duration, t.file_path,
-	t.genre_id, t.stream_count,
-	m.name AS artist, a.cover_path, t.visibility, t.title_lower, m.name_lower
+	SELECT 
+    t.id, t.title, t.musician_id, m.name, a.cover_path, 
+    t.file_path, t.duration, t.stream_count, t.visibility
 	FROM track t
 	JOIN musician m ON t.musician_id = m.id
 	LEFT JOIN album a ON t.album_id = a.id
-	WHERE t.title_lower LIKE ? OR m.name_lower LIKE ? AND t.visibility = 'public'
+	WHERE 
+    (t.title_lower LIKE ? OR m.name_lower LIKE ?)
+    AND t.visibility = 'public'
 	LIMIT 50;
 	`
 
@@ -119,8 +121,8 @@ func (handler *SearchHandler) SearchTracks(response http.ResponseWriter, request
 	for rows.Next() {
 		var tr models.TrackResponse
 		if err := rows.Scan(
-			&tr.ID, &tr.ArtistID, &tr.Title, &tr.Duration, &tr.AudioURL, &tr.Plays,
-			&tr.ArtistName, &tr.ImageURL, &tr.Visibility,
+			&tr.ID, &tr.Title, &tr.ArtistID, &tr.ArtistName, &tr.ImageURL,
+			&tr.AudioURL, &tr.Duration, &tr.Plays, &tr.Visibility,
 		); err != nil {
 			log.Println("SearchTracks: ", err)
 			http.Error(response, err.Error(), http.StatusInternalServerError)
