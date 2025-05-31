ea9a25a1274686ff97b5e66dba725d6444202e17
Vlasov Danil <asingleextrasecond@gmail.com>
2025-06-01 03:23:08 +0800

    fixed search handler, added logs

--- handlers/search.go
+++ handlers/search.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/Edafi/MusicVibe/models"
 )
@@ -15,8 +17,8 @@ type SearchHandler struct {
 func (handler *SearchHandler) GetNewTracks(response http.ResponseWriter, request *http.Request) {
 	query := `
 	SELECT t.id, t.musician_id, t.album_id, t.title, t.duration, t.file_path,
-	       t.genre_id, t.stream_count,
-	       m.name AS artist, a.cover_path, t.visibility,
+	t.genre_id, t.stream_count,
+	m.name AS artist, a.cover_path, t.visibility,
 	FROM track t
 	JOIN musician m ON t.musician_id = m.id
 	LEFT JOIN album a ON t.album_id = a.id
@@ -27,6 +29,7 @@ func (handler *SearchHandler) GetNewTracks(response http.ResponseWriter, request
 
 	rows, err := handler.DB.Query(query)
 	if err != nil {
+		log.Println("GetNewTracks: ", err)
 		http.Error(response, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -39,6 +42,7 @@ func (handler *SearchHandler) GetNewTracks(response http.ResponseWriter, request
 			&tr.ID, &tr.ArtistID, &tr.Title, &tr.Duration, &tr.AudioURL, &tr.Plays,
 			&tr.ArtistName, &tr.ImageURL, &tr.Visibility,
 		); err != nil {
+			log.Println("GetNewTracks: ", err)
 			http.Error(response, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -62,6 +66,7 @@ func (handler *SearchHandler) GetChartTracks(response http.ResponseWriter, reque
 
 	rows, err := handler.DB.Query(query)
 	if err != nil {
+		log.Println("GetChartTracks: ", err)
 		http.Error(response, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -74,6 +79,7 @@ func (handler *SearchHandler) GetChartTracks(response http.ResponseWriter, reque
 			&tr.ID, &tr.ArtistID, &tr.Title, &tr.Duration, &tr.AudioURL, &tr.Plays,
 			&tr.ArtistName, &tr.ImageURL, &tr.Visibility,
 		); err != nil {
+			log.Println("GetChartTracks: ", err)
 			http.Error(response, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -83,26 +89,27 @@ func (handler *SearchHandler) GetChartTracks(response http.ResponseWriter, reque
 }
 
 func (handler *SearchHandler) SearchTracks(response http.ResponseWriter, request *http.Request) {
-	q := request.URL.Query().Get("query")
+	q := request.URL.Query().Get("q")
 	if q == "" {
+		log.Println("SearchTracks: q is not valid")
 		http.Error(response, "Missing query parameter", http.StatusBadRequest)
 		return
 	}
-
 	query := `
 	SELECT t.id, t.musician_id, t.album_id, t.title, t.duration, t.file_path,
 	t.genre_id, t.stream_count,
-	m.name AS artist, a.cover_path, t.visibility,
+	m.name AS artist, a.cover_path, t.visibility, t.title_lower, m.name_lower
 	FROM track t
 	JOIN musician m ON t.musician_id = m.id
 	LEFT JOIN album a ON t.album_id = a.id
-	WHERE t.title LIKE ? OR m.name LIKE ? AND t.visibility = 'public'
-	LIMIT 20;
+	WHERE t.title_lower LIKE ? OR m.name_lower LIKE ? AND t.visibility = 'public'
+	LIMIT 50;
 	`
 
 	likePattern := "%" + q + "%"
-	rows, err := handler.DB.Query(query, likePattern, likePattern)
+	rows, err := handler.DB.Query(query, strings.ToLower(likePattern), strings.ToLower(likePattern))
 	if err != nil {
+		log.Println("SearchTracks: ", err)
 		http.Error(response, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -115,6 +122,7 @@ func (handler *SearchHandler) SearchTracks(response http.ResponseWriter, request
 			&tr.ID, &tr.ArtistID, &tr.Title, &tr.Duration, &tr.AudioURL, &tr.Plays,
 			&tr.ArtistName, &tr.ImageURL, &tr.Visibility,
 		); err != nil {
+			log.Println("SearchTracks: ", err)
 			http.Error(response, err.Error(), http.StatusInternalServerError)
 			return
 		}
