ac22ca89aba94d05fb45d6deed5b832e1b18208f
Vlasov Danil <asingleextrasecond@gmail.com>
2025-06-09 00:16:21 +0800

    fixed following json response

--- handlers/following.go
+++ handlers/following.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 
 	"github.com/Edafi/MusicVibe/middleware"
-	"github.com/Edafi/MusicVibe/models"
 	"github.com/gorilla/mux"
 )
 
@@ -17,10 +16,6 @@ type FollowingHandler struct {
 
 // Получить подписанных музыкантов
 func (handler *FollowingHandler) GetFollowingMusicians(response http.ResponseWriter, request *http.Request) {
-
-	log.Println("Path:", request.URL.Path)
-	log.Println("Vars:", mux.Vars(request))
-
 	val := request.Context().Value(middleware.ContextUserIDKey)
 	userID, ok := val.(string)
 	if !ok {
@@ -29,78 +24,31 @@ func (handler *FollowingHandler) GetFollowingMusicians(response http.ResponseWri
 		return
 	}
 
-	log.Println("User ID:", userID)
-
 	rows, err := handler.DB.Query(`
-		SELECT m.id, u.email, m.name, u.avatar_path, u.background_path, 
-		u.description, u.has_complete_setup
+		SELECT m.id
 		FROM user_following uf
 		JOIN musician m ON uf.musician_id = m.id
-		JOIN user u ON u.id = m.user_id
 		WHERE uf.user_id = ?`, userID)
 	if err != nil {
-		http.Error(response, err.Error(), http.StatusInternalServerError)
+		log.Println("GetFollowingMusicians - DB Query error:", err)
+		http.Error(response, "Failed to load followed musicians", http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
 
-	var musicians []models.MusicianPage = make([]models.MusicianPage, 0)
+	var musicianIDs []string
 	for rows.Next() {
-		var m models.MusicianPage
-		err := rows.Scan(&m.ID, &m.Email, &m.Name, &m.AvatarPath, &m.BackgroundPath, &m.Description, &m.HasCompleteSetup)
-		if err != nil {
-			http.Error(response, err.Error(), http.StatusInternalServerError)
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			log.Println("GetFollowingMusicians - Row Scan error:", err)
+			http.Error(response, "Failed to parse musician ID", http.StatusInternalServerError)
 			return
 		}
-
-		// Жанры
-		genreRows, err := handler.DB.Query(`
-			SELECT g.name 
-			FROM musician_genre mg 
-			JOIN genre g ON g.id = mg.genre_id 
-			WHERE mg.musician_id = ?`, m.ID)
-		if err == nil {
-			for genreRows.Next() {
-				var genre string
-				genreRows.Scan(&genre)
-				m.Genres = append(m.Genres, genre)
-			}
-			genreRows.Close()
-		}
-
-		// Социальные ссылки
-		socialRows, err := handler.DB.Query(`
-			SELECT name, url 
-			FROM musician_social_link 
-			WHERE musician_id = ?`, m.ID)
-		if err == nil {
-			for socialRows.Next() {
-				var link models.SocialLink
-				socialRows.Scan(&link.Name, &link.URL)
-				m.SocialLinks = append(m.SocialLinks, link)
-			}
-			socialRows.Close()
-		}
-
-		// Альбомы (предпросмотр)
-		albumRows, err := handler.DB.Query(`
-			SELECT id, title, cover_path 
-			FROM album 
-			WHERE musician_id = ?`, m.ID)
-		if err == nil {
-			for albumRows.Next() {
-				var album models.AlbumPreview
-				albumRows.Scan(&album.ID, &album.Title, &album.CoverUrl)
-				m.Albums = append(m.Albums, album)
-			}
-			albumRows.Close()
-		}
-
-		musicians = append(musicians, m)
+		musicianIDs = append(musicianIDs, id)
 	}
 
 	response.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(response).Encode(musicians)
+	json.NewEncoder(response).Encode(musicianIDs)
 }
 
 // Подписаться
